b001741ac2a1b27d82cfb43ef8dd79c92a5c9230
sagartiwari0608 <sagartiwarisrt@gmail.com>
2025-01-20 01:34:00 +0530

    comments added for method implementations

--- src/main/java/com/JPA/JpaApplication.java
+++ src/main/java/com/JPA/JpaApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.util.Optional;
+
 @SpringBootApplication
 public class JpaApplication {
 
@@ -16,8 +18,19 @@ public class JpaApplication {
 		s1.setAge(18);
 		s1.setName("Sagar");
 		s1.setRollNo(1);
-		repo.save(s1);
+		repo.save(s1); // now this along with many other methods are already available in jparepository we need not bother to implement basic one's and we can focus on the specific one's our application needs.
+
+		Student s2 = context.getBean(Student.class);
+		s2.setAge(19);
+		s2.setName("Akshata");
+		s2.setRollNo(2);
+		repo.save(s2); // now this along with many other methods are already available in jparepository we need not bother to implement basic one's and we can focus on the specific one's our application needs.
 		System.out.println(repo.findAll());
+
+		Optional<Student> s3 = repo.findById(105); // for many methods like this one we need to write Optional because they return Optional<T> why because we might ask for data which is not there at all and in that case.
+//		we'll get null in return and these might lead to null pointer exceptions.which are not good
+		System.out.println(repo.findByName("Sagar")); // now for the methods which don't exist already. we need to implement as we did.
+		System.out.println(repo.findByAgeGreaterThan(18));
 	}
 
 }
--- src/main/java/com/JPA/Repositories/StudentRepo.java
+++ src/main/java/com/JPA/Repositories/StudentRepo.java
@@ -2,7 +2,18 @@ package com.JPA.Repositories;
 
 import com.JPA.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface StudentRepo extends JpaRepository<Student, Integer> {
-//    here we need not implement generic methods like findAll save etc. these will be handled if we have something too specific we can always go ahead and create methods.
+//    here we need not implement generic methods like findAll save etc. these will be handled
+//    if we have something too specific we can always go ahead and create methods.
+//    @Query("select s from Student s where name= ?1") // we need to mention Student as class and object s from it
+//    The query written above is also unnecessary as JPA has DSL domain specific language even if the queries are not present but if they are related to variables/properties of the entity then they willl be created.
+    List<Student> findByName(String name);
+//    And also if the method name makes sense and uses common sense
+    List<Student> findByAgeGreaterThan(int age);
+
+
 }
